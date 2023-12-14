07ef33543ecb3db66ce04457aa347d319b5baa3b
Kutluay <kutluay3344@hotmail.com>
2023-12-14 16:23:30 +0300

    jwtTokenApp

---
+++ jwtTokenApp/src/main/java/com/kutluay/jwtTokenApp/JwtTokenAppApplication.java
@@ -0,0 +1,13 @@
+package com.kutluay.jwtTokenApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenAppApplication.class, args);
+	}
+
+}
---
+++ jwtTokenApp/src/test/java/com/kutluay/jwtTokenApp/JwtTokenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutluay.jwtTokenApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
